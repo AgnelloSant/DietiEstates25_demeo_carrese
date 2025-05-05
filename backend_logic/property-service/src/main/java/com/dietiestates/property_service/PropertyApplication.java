169ca9d8553ce0b243c62d72cdc7592e5b0acdc3
davidecr03 <153207173+davidecr03@users.noreply.github.com>
2025-05-05 17:54:15 +0200

    aggiornamento funzionamento logica di uodate e search+ correzione del collegamento a supabase

--- backend_logic/property-service/src/main/java/com/dietiestates/property_service/PropertyApplication.java
+++ backend_logic/property-service/src/main/java/com/dietiestates/property_service/PropertyApplication.java
@@ -1,4 +1,4 @@
-package com.dietiestates.property_service.PropertyServiceApplication;
+package com.dietiestates.property_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
