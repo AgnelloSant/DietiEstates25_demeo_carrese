ba803c938e4fc8c4b92a4810978f76d870d10bce
Pierluigi De Meo <pierludm2002@gmail.com>
2026-02-14 17:43:18 +0100

    general deploy

--- backend/api-gateway/src/main/java/com/backend/config/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/backend/config/GatewayConfig.java
@@ -20,14 +20,14 @@ public class GatewayConfig {
                                 .route("user-service", r -> r.path("/user/**")
                                                 .filters(f -> f.filter(authFilter.apply(
                                                                 new com.backend.filter.AuthenticationFilter.Config())))
-                                                .uri("http://user-service:8080"))
+                                                .uri("http://user-service.dietiestates.local:8080"))
                                 .route("auth-service", r -> r.path("/auth/**")
                                                 .filters(f -> f.prefixPath("/user"))
-                                                .uri("http://user-service:8080"))
+                                                .uri("http://user-service.dietiestates.local:8080"))
                                 .route("property-service", r -> r.path("/properties/**")
                                                 .filters(f -> f.filter(authFilter.apply(
                                                                 new com.backend.filter.AuthenticationFilter.Config())))
-                                                .uri("http://property-service:8081"))
+                                                .uri("http://property-service.dietiestates.local:8081"))
                                 .build();
         }
 
